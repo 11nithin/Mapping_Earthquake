4ae4e7ff233893f08a3528c19bc272c8f00afd82
11nithin <nithinjacobjames@hotmail.com>
2021-11-26 15:54:07 -0500

    fill color

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -8,7 +8,7 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 L.circleMarker([34.0522, -118.2437],{
     radius: 300,
     color: "black",
-    fillcolor: "#ffffa1"
+    fillColor: "#ffffa1"
 }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
