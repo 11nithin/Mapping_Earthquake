9367b291f290d4bbca7543a9195d28d298d7a99b
11nithin <nithinjacobjames@hotmail.com>
2021-11-28 12:31:22 -0500

    Update logic.js

--- Mapping multiple geojsondata/static/js/logic.js
+++ Mapping multiple geojsondata/static/js/logic.js
@@ -2,10 +2,10 @@
 console.log("working");
 
 // Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+let map = L.map('mapid').setView([44.0, -80.0], 2);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -15,16 +15,19 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 streets.addTo(map);
 
 // Coordinates for each point to be used in the line.
-let airportData = "https://raw.githubusercontent.com/11nithin/Mapping_Earthquake/main/majorAirports.json"
+let torontoData = "https://raw.githubusercontent.com/11nithin/Mapping_Earthquake/Mapping_GeoJSON_Linestrings/torontoRoutes.json"
 
 
  // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
- console.log(data);
-
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).bindPopup("<h2>City:" + data.features[0].properties.city+ "</h2><hr><h3>Airport code:" + data.features[0].properties.faa +" </h3>"  ).addTo(map);
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data,{
+      color: "yellow"
+  }).bindPopup("<h2>Airline:" + data.features[0].properties.airline+ "</h2><hr><h3>Airline code:" + data.features[0].properties.airline_id +" </h3>"  ).addTo(map);
 });
+
 
 
 
