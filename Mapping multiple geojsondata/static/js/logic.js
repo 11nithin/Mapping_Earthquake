f27521ccae022b58bb656e642e7636218385f9cb
11nithin <nithinjacobjames@hotmail.com>
2021-11-28 12:12:23 -0500

    cv

--- Mapping multiple geojsondata/static/js/logic.js
+++ Mapping multiple geojsondata/static/js/logic.js
@@ -1,31 +1,31 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([36.1733, -120.1794], 5);
-
-// Coordinates for each point to be used in the line.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790], 
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
-
-  // Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "yellow"
-  }).addTo(map);
-
-
-// Loop through the cities array and create one marker for each city.
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 14,
+    maxZoom: 18,
     accessToken: API_KEY
 });
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Coordinates for each point to be used in the line.
+let airportData = "https://raw.githubusercontent.com/11nithin/Mapping_Earthquake/main/majorAirports.json"
+
+
+ // Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+ console.log(data);
+
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).bindPopup("<h2>City:" + data.features[0].properties.city+ "</h2><hr><h3>Airport code:" + data.features[0].properties.faa +" </h3>"  ).addTo(map);
+});
+
+
+
+
